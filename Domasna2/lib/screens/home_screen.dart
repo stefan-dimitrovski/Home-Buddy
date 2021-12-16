f165cd474e7172f6d863ae2fd5a3e95a9bac1eaf
stefan <stefan.dimitrovski_19@hotmail.com>
2021-12-16 16:49:26 +0100

    list view placeholder implementation

--- Domasna2/lib/screens/home_screen.dart
+++ Domasna2/lib/screens/home_screen.dart
@@ -43,15 +43,76 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
+      //TODO: Create a seperate widget for this
       body: ListView.builder(
         itemCount: 10,
-        itemBuilder: (context, index) => Center(
-          child: Card(
+        itemBuilder: (context, index) => ListTile(
+          title: Card(
+            elevation: 5,
             child: Container(
               width: double.infinity,
-              height: 50,
+              height: 160,
               margin: const EdgeInsets.all(8),
-              child: const Text("Example List Item"),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.room_outlined,
+                            color: Colors.black45,
+                          ),
+                          Text('London'),
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.apartment_outlined,
+                            color: Colors.black45,
+                          ),
+                          Text('Apartment'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                        margin: const EdgeInsets.all(8),
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.network(
+                            'https://picsum.photos/500?image=$index',
+                            fit: BoxFit.cover,
+                            width: 200,
+                            height: 100,
+                          ),
+                        )),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Text("\$205"),
+                          Text('/ night'),
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          Text(
+                            '1 bed • 1 bath',
+                            style: TextStyle(color: Colors.black26),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
