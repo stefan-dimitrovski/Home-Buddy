dc263b48a80ae8f8c284cdafe192a5c7d3b8d671
stefan <stefan.dimitrovski_19@hotmail.com>
2021-12-16 15:56:29 +0100

    initial look

--- Domasna2/lib/screens/home_screen.dart
+++ Domasna2/lib/screens/home_screen.dart
@@ -7,16 +7,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Container(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore_outlined),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border_rounded),
+            label: 'Favorites',
+          ),
+        ],
+      ),
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Explore'),
         actions: [
+          IconButton(
+            icon: const Icon(Icons.filter_alt_outlined),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.add_box_outlined),
+            onPressed: () {},
+          ),
           IconButton(
             onPressed: () async => {await FirebaseAuth.instance.signOut()},
             icon: const Icon(Icons.logout),
           ),
         ],
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) => Center(
+          child: Card(
+            child: Container(
+              width: double.infinity,
+              height: 50,
+              margin: const EdgeInsets.all(8),
+              child: const Text("Example List Item"),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
