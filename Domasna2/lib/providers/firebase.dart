f8c7fa52ef1453ea2eca38c3a464a163185ddffd
stefan <stefan.dimitrovski_19@hotmail.com>
2022-03-31 21:14:50 +0200

    singleton fix;

--- Domasna2/lib/providers/firebase.dart
+++ Domasna2/lib/providers/firebase.dart
@@ -3,14 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreInstance {
   static FirebaseFirestore? _instance;
 
-  FirestoreInstance._internal() {
-    FirebaseFirestore.instance;
-  }
-
   static FirebaseFirestore? get instance {
-    // ignore: prefer_conditional_assignment
     if (_instance == null) {
-      _instance = FirestoreInstance._internal() as FirebaseFirestore;
+      _instance = FirebaseFirestore.instance;
     }
     return _instance;
   }
